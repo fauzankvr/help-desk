e91f7e9ac7fa4f36963250e2e946440414858a4f
fauzankvr <muhammedfauzan7862@gmail.com>
2025-09-23 20:03:46 +0530

    chage: index.cjs

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "src/index.js",
+      entry: "src/index.js", // entry file of your package
       name: "HelpDesk",
-      fileName: "index",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"], // 👈 generate BOTH esm and cjs
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom"], // don’t bundle React
       output: {
         globals: {
           react: "React",
